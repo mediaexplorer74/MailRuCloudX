7754d8b3055f9cbfdc99581b26c054b7b057b33a
Andrey Zakharov <mediaexplorer74@hotmail.com>
2023-03-25 08:57:49 +0300

    .

---
+++ MailRuCloudX/MailRuCloudX/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MailRuCloudApi
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Login = "myrauthorization@mail.ru"; // test login
+        private const string Password = "190190aA"; // test pwd
+        private Account account = new Account(Login, Password);
+
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+
+        public void A1LoginTest()
+        {
+            account.Login();
+            Debug.WriteLine("[i] account.AuthToken = " + account.AuthToken);
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            A1LoginTest();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# MailRuCloudX  / "MailRuCloud мульти-клиент"
+
+MailRuCloud (cloud.mail.ru) simple Xamarin Forms sample app (template). Штука для взаимодействия с MailRu Cloud через API. 
+Была сделана по причине устаревания стандартного приложения Облако МайлРу для винфонов (оно тупо не даёт залогиниться 
+и войти, а значит и работать с файло/папками).
+
+RU only. Только для России.
+
+
+## Current Status
+- New version (or "iteration") with minimal improvements ("null value & exceptions" bug detected & fixed)
+- Ready Targets: UWP, W10 or 11 (min. os. build 16299 at now)
+- Not ready/not tested Targets/Platforms: iOS and Android
+
+## TODO
+- Add .net 1.4 lib for "15063" compatibility.
+- Add some UI
+
+## .
+
+AS IS. Как есть.
+
+[m][e] 2022-2023
+
